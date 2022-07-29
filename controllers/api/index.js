602f523ab7607af189d8562aee4e8974ea51d642
Ricma <ric.madse@gmail.com>
2022-07-28 19:18:58 -0600

    pushing routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,6 @@
 const router = require('express').Router();
+const userRoutes = require('./resumeRoutes')
+
+router.use('/resume', resumeRoutes)
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,11 +4,21 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
     try {
         console.log('there')
-        res.render('login')
+        res.render('homepage')
 
     } catch (err) {
         res.status(500).json(err);
       }
 })
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
